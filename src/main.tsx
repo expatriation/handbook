9f93faea704f8cad9c6f14916ce4efe21d9b1a91
candidbook <veripax@outlook.com>
2026-04-14 23:18:39 +0000

    polyfill global Buffer for bip39 in vite runtime

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Buffer } from 'buffer';
 import App from './app';
+
+if (!globalThis.Buffer) {
+  globalThis.Buffer = Buffer;
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
